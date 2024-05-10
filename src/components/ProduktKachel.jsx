b8572eddd7076608f0cbbc2eb77e59c89c43e454
slarixxa <larissaaa2020@gmail.com>
2024-05-10 15:41:19 +0200

    verusch vom warenkorb

--- src/components/ProduktKachel.jsx
+++ src/components/ProduktKachel.jsx
@@ -1,11 +1,35 @@
-const ProduktKachel = ({ produktname, produktBild, produktbeschreibung }) => {
+import PropTypes from "prop-types";
+
+const ProduktKachel = ({ produktInfo, onAddToCart }) => {
+  const { produktname, produktBild, produktbeschreibung } = produktInfo;
+
+  const handleAddToCart = () => {
+    onAddToCart(produktInfo);
+    console.log("Add to cart clicked:", produktInfo);
+  };
+
+  //const ProduktKachel = ({ produktname, produktBild, produktbeschreibung }) => {
   return (
     <article className="border-black border">
       <h1>{produktname}</h1>
       <img src={produktBild}></img>
       <p>{produktbeschreibung}</p>
-      <button className="border rounded-md bg-orange-600">Einfügen</button>
+      <button
+        className="border rounded-md bg-orange-600"
+        onClick={handleAddToCart}
+      >
+        Einfügen
+      </button>
     </article>
   );
+};
+
+ProduktKachel.propTypes = {
+  produktInfo: PropTypes.shape({
+    produktname: PropTypes.string.isRequired,
+    produktBild: PropTypes.string.isRequired,
+    produktbeschreibung: PropTypes.string.isRequired,
+  }).isRequired,
+  onAddToCart: PropTypes.func.isRequired,
 };
 export default ProduktKachel;
--- src/components/Produkte.jsx
+++ src/components/Produkte.jsx
@@ -1,17 +1,32 @@
 import ProduktKachel from "./ProduktKachel";
 import produktejson from "../mocks/produkte.json";
-const Produkte = () => {
+import PropTypes from "prop-types";
+
+const Produkte = ({ addToCart }) => {
+  const handleAddToCart = (produkt) => {
+    // Implementiere die Logik zum Hinzufügen des Produkts zum Warenkorb hier
+    console.log("Produkt wurde zum Warenkorb hinzugefügt:", produkt);
+    addToCart(produkt);
+  };
   return (
     <div className="grid grid-cols-3 gap-2">
-      {produktejson.map((produkt) => (
+      {produktejson.map((produkt, index) => (
         <ProduktKachel
-          produktname={produkt.produktname}
-          produktBild={produkt.produktBild}
-          produktbeschreibung={produkt.produktbeschreibung}
-          key={produkt.produktname}
+          // produktname={produkt.produktname}
+          //  produktBild={produkt.produktBild}
+          //  produktbeschreibung={produkt.produktbeschreibung}
+          // key={produkt.produktname}
+          key={index}
+          //produktInfo={{ ...produkt, onAddToCart: addToCart }}
+          produktInfo={{ ...produkt }}
+          onAddToCart={handleAddToCart}
         />
       ))}
     </div>
   );
 };
+Produkte.propTypes = {
+  addToCart: PropTypes.func.isRequired,
+};
+
 export default Produkte;
